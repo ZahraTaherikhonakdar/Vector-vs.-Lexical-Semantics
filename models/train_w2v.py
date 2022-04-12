605385961ac5258b89833d76d4fd2bc42d5ddcae
ZahraTaherikhonakdar <75437827+ZahraTaherikhonakdar@users.noreply.github.com>
2022-04-12 00:44:56 -0400

    Add files via upload

--- models/train_w2v.py
+++ models/train_w2v.py
@@ -1,1 +1,7 @@
-gitkeep
+from gensim.models import word2vec, keyedvectors, Word2Vec
+
+
+def train_model(text_vec, path,name):
+            model = Word2Vec(text_vec, min_count=1, window=50,epochs=5, vector_size=100, sg=1)
+            model.save("{}/w2v_{}".format(path,name))
+            print(f'saved w2v model ...\n {path}/w2v_{name}')
